5f221649c208b1280f90f59b8b192dfd51cc9b68
Amanda Rolon <abrolon87@gmail.com>
2019-12-23 16:35:38 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -4,11 +4,14 @@ class Cat #= Struct.new(:attr_names) do
   def name=(name)
     @name = name
   end
-  
+
   def name
     @name
   end
 
+  attr_accessor :name
+
+  end
   def meow
     puts "meow!"
   end
